083ba409bd4b5fdc1afabb24c51d9ea6c240da18
mcgnexus <mcgnexus@gmail.com>
2026-09-19 22:02:47 +0200

    fix(build): declarar config de middleware estático (re-export roto en Next 15)

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,13 @@
-export { middleware, config } from "./src/middleware";
+export { middleware } from "./src/middleware";
+
+export const config = {
+  matcher: [
+    "/:path*",
+    "/api/:path*",
+    "/_next/static/:path*",
+    "/_next/image/:path*",
+    "/favicon.ico",
+    "/robots.txt",
+    "/sitemap.xml",
+  ],
+};
